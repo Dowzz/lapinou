48af09d6215bfb9b10d76be0d6b3b8afe7111d24
NoemieM <temyline@gmail.com>
2017-04-14 15:31:57 +0200

    Miniature

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -93,8 +93,16 @@
     </div>
 </div>
 
-<!--Scroll article interaction-->
-<img src="{{URL::asset('/img/fille.png')}}" alt="">
+<!--Miniature-->
+<div class="view">  
+     <img src="{{URL::asset('/img/fille.png')}}" alt=""> 
+     <div class="mask">  
+     <h2>Une fille parfaite</h2>  
+     <p>«Je la suis depuis plusieurs jours. Je sais où elle fait ses courses, où elle travaille. Je ne connais pas la couleur de ses yeux ni comment est son regard quand elle a peur. Mais je le saurai bientôt.»</p>  
+         <a href="#" class="info">Voir plus</a>  
+     </div>  
+</div>  
+
 
 
 </body>
