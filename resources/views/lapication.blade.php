07d80f230436b370955c6b9a4b8e3f21d87caecf
Dowzz <michel.barny@outlook.fr>
2017-04-14 14:35:37 +0200

    lapication

---
+++ resources/views/lapication.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>lapication</title>
+</head>
+<body>
+<h1>lapi'cation est une application gratuite vous permettant d'acceder directement a votre bibliotheque contenant les livres que vous avez acheté sur notre site.</h1>
+
+<a href="#" id="download">Téléchargement</a>
+</body>
+</html>
