d837858aa26d6b46b24d58e50a71614d737e5451
NoemieM <temyline@gmail.com>
2017-04-18 12:08:49 +0200

    Footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -2,8 +2,51 @@
 <script src="{{ elixir('js/all.js') }}"></script>
 
 
-<div class="row">
-	<div>
-		<h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi cum perspiciatis, similique officiis. Minima aspernatur ratione velit consequuntur doloribus iure, eligendi enim, fugiat reiciendis corporis minus quibusdam expedita repellat iste.</h2>
-	</div>
-</div>
+<footer>
+    <div class="footer" id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h3> Contact </h3>
+                    <ul>
+                        <li> Lapibook </li>
+                        <li> 7 rue des Carottes </li>
+                        <li> 00 000 Rabbit City </li></ul></br>
+                        <li> <a href="#"> contact@lapibook.fr </a> </li></br>
+                        <li> 02 31 00 00 00 </li></br>
+                        <li> <a href="#"> Contactez-nous </a> </li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <h3> Liens </h3>
+                    <ul>
+                        <li> <a href="#"> Accueil </a> </li>
+                        <li> <a href="#"> Catégorie </a> </li>
+                        <li> <a href="#"> Connexion </a> </li>
+                        <li> <a href="#"> Inscription </a> </li>                   
+                        <li> <a href="#"> Lapi'book </a> </li>
+                        <li> <a href="#"> Panier </a> </li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <h3> Mentions Légales </h3>
+                    <ul>
+                        <li> <a href="#"> Politique de confidentialité </a> </li>
+                        <li> <a href="#"> CGV </a> </li>
+                        <li> <a href="#"> Cookies </a> </li>
+                    </ul>
+                </div>
+            </div>
+            <!--/.row-->
+        </div>
+        <!--/.container-->
+    </div>
+    <!--/.footer-->
+
+    <div class="footer-bottom">
+        <div class="container">
+            <p class="pull-left"> Copyright © 2017, LAPIBOOK. All rights reserved.</p>
+        </div>
+    </div>
+    <!--/.footer-bottom-->
+</footer>
