ca6a5905d6fd610cddcf9591ce647ad802f770be
NoemieM <temyline@gmail.com>
2017-04-19 16:38:37 +0200

    Profil 1.0

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -1,8 +1,39 @@
 
-
-
-
-	<div class="row">
-		<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum tempore suscipit distinctio asperiores, reprehenderit, iste aspernatur ut placeat sapiente accusamus voluptate repudiandae? Eius ipsam, et commodi, tempora deleniti sed necessitatibus.</h1>
-	</div>
+<div class="container profile">
+  <div class="row">
+    <div class="span12">
+      <div class="well well-small clearfix">
+        <div class="row-fluid">
+          <div class="span2">
+             <img src="{{URL::asset('/img/social.png')}}" class="img-polaroid"/>         
+          </div>
+          <div class="span4">
+             <h2>Lapinou</h2> 
+            <ul class="unstyled">
+              <li>Lapinou@mail.com</li>
+              <li>Né(e) le 25 septembre 2000</li>
+              <li>Incription le 14 février 2011</li>
+            </ul>
+          </div>
+          <div class="span6">
+          <ul class="inline stats">
+               <li>
+                <span>27</span>
+                Livres
+              </li>
+              <li>
+                <span>12</span>
+                Livres à lire
+              </li>
+               <li>
+                <span>15</span>
+                Livres lus
+              </li>
+          </ul>
+            <div><!--/span6-->
+            </div><!--/row-->
+      </div>
+      <!--Body content-->
+    </div>
+  </div>
 
