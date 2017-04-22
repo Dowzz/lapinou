86303beda102db060ca8078902c4acce27a9e667
Dowzz <michel.barny@outlook.fr>
2017-04-22 16:46:29 +0200

    probleme resolu

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -43,7 +43,7 @@
 
 <!-- Large modal -->
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-sm">
+    <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
