31311cad02c595861693aa1dcc2ed7d27c5615a5
NoemieM <temyline@gmail.com>
2017-04-24 12:33:12 +0200

    Search box + indentation

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -41,7 +41,17 @@
     </div>
 </div>
 
-
+<!--Barre de recherche-->
+<div class="flexsearch">
+        <div class="flexsearch--wrapper">
+            <form class="flexsearch--form" action="#" method="post">
+                <div class="flexsearch--input-wrapper">
+                    <input class="flexsearch--input" type="search" placeholder="Recherche">
+                </div>
+                <input class="flexsearch--submit" type="submit" value="&#10140;"/>
+            </form>
+        </div>
+</div>
 
 <!-- Large modal -->
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
