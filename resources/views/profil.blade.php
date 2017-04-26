284f906c40d5d2a13be032f5210c725e27403c3f
NoemieM <temyline@gmail.com>
2017-04-26 14:14:27 +0200

    Profil + image lapin

---
+++ resources/views/profil.blade.php
@@ -0,0 +1,145 @@
+
+    <div class="container profile">
+      <div class="span12">
+        <div class="well well-small clearfix">
+
+            <div class="span2">
+               <img src="{{URL::asset('/img/profil.jpg')}}" class="img-polaroid"/>
+            </div>
+            <div class="span4">
+               <h2>Lapinou</h2> 
+                  <ul class="unstyled">
+                    <li>Lapinou@mail.com</li>
+                    <li>Né(e) le 25 septembre 2000</li>
+                    <li>Incription le 14 février 2011</li>
+                  </ul>
+            </div>
+            <div class="span6">
+            <ul class="inline stats">
+                 <li class="livres">
+                  <span>27</span>
+                  Livres
+                </li>
+                <li class="livres">
+                  <span>12</span>
+                  Livres à lire
+                </li>
+                <li class="livres">
+                  <span>15</span>
+                  Livres lus
+                </li>
+            </ul>
+        </div>
+     </div>
+  </div>
+  </div>
+
+
+  <div class="col-md-9">
+  <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingTwo">
+      <h4 class="panel-title">
+        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+          Mes informations personnelles
+        </a>
+      </h4>
+    </div>
+    <div id="collapseTwo" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingTwo">
+
+        <div class="form-group">
+            <label class="col-sm-2 control-label" for="textinput">Nom</label>
+            <div class="col-sm-10">
+              <input type="text" placeholder="Nom" class="form-control">
+            </div>
+          </div>      
+          <div class="form-group">
+            <label class="col-sm-2 control-label" for="textinput">Prénom</label>
+            <div class="col-sm-10">
+              <input type="text" placeholder="Prénom" class="form-control">
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="col-sm-2 control-label" for="textinput">Email</label>
+            <div class="col-sm-10">
+              <input type="email" placeholder="Email" class="form-control">
+            </div>
+          </div>
+
+
+            
+
+          <div class="form-group">
+            <label class="col-sm-2 control-label" for="textinput">Pays</label>
+            <div class="col-sm-10">
+              <input type="text" placeholder="Pays" class="form-control">
+            </div>
+          </div>
+
+
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingThree">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          Mes commandes
+        </a>
+      </h4>
+    </div>
+    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+      <div class="panel-body">
+        
+
+  <table class="table table-xs">
+    <tr>
+      <th> Titre du livre </th>
+      <th class="text-right"> Date </th>
+      <th class="text-right"> Prix </th>
+    </tr>
+    <tr class="item-row">
+        <p> Une fille parfaite </p>
+
+
+
+
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingFour">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+          Mes avis
+        </a>
+      </h4>
+    </div>
+    <div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
+      <div class="panel-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingFive">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+          Mes paramètres
+        </a>
+      </h4>
+    </div>
+    <div id="collapseFive" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFive">
+      <div class="panel-body">
+        - To do list Y/N <br />
+        - Newletter Y/N <br />
+        - Désactivation de compte <br />
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+
