cec3e58dd73bcefeb309497b685e0cbcacd47fed
Dowzz <michel.barny@outlook.fr>
2017-05-09 15:32:40 +0200

    ooops

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,8 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->integer('todo');
+            $table->integer('admin');
         });
     }
 
