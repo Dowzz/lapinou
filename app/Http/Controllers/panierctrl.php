cae257c74414ed4c7fb56be560ffcfbd89ff48df
Dowzz <michel.barny@outlook.fr>
2017-05-19 16:50:05 +0200

    commit panier

--- app/Http/Controllers/panierctrl.php
+++ app/Http/Controllers/panierctrl.php
@@ -6,29 +6,26 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\panier;
+use App\commande;
 
 class panierctrl extends Controller
 {
     public function addpanier(Request $request){
-    	$panier = $request->session()->get('Ncommande');
-    	if($panier !=null){
-		$panier->couverture = "";
-    	$panier->titre = "";
-    	$panier->auteur = "";
-    	$panier->format = "";
-    	$panier->prix = "";
-    	$panier->save();
-
-
-    	}else{
-			$panier = new panier;
-			$panier->couverture = "";
-    		$panier->titre = "";
-    		$panier->auteur = "";
-    		$panier->format = "";
-    		$panier->prix = "";
-    		$panier->save();
-    	}
+    	$panier = $request->session()->get('idpanier');
+    	if($panier = null){
+            $panier = new panier;
+        }            
+            $commande= new commande;
+            $commande->couverture ="test"; 
+            $commande->titre ="test";
+            $commande->auteur ="test"; 
+            $commande->format ="test"; 
+            $commande->prix ="0.1"; 
+            $commande->save();
+            $panier->Ncommande=$this->$commande->Ncommande;
+            $panier->save();
+            
+        }
     	
     }
-}
+
--- database/migrations/2017_05_19_125633_create_commandes_table.php
+++ database/migrations/2017_05_19_125633_create_commandes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePaniersTable extends Migration
+class CreateCommandesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,13 @@ class CreatePaniersTable extends Migration
      */
     public function up()
     {
-        Schema::create('paniers', function (Blueprint $table) {
-            $table->increments('idpanier');
-            $table->integer('Ncommande');
+        Schema::create('commandes', function (Blueprint $table) {
+            $table->increments('Ncommande');
+            $table->text('couverture');
+            $table->text('titre');
+            $table->text('auteur');
+            $table->text('format');
+            $table->double('prix');
    
         });
     }
@@ -27,6 +31,6 @@ class CreatePaniersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('paniers');
+        Schema::drop('commandes');
     }
 }
